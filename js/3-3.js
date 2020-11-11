d3a35b2452221da0c47362f17726ee27f3f8617b
Dmytro <mityaua@gmail.com>
2020-11-11 17:06:38 +0200

    Добавляет второй вариант решения 3й задачи

--- js/3-3.js
+++ js/3-3.js
@@ -50,3 +50,51 @@ const sellers = {
 };
 console.log(findBestEmployee(sellers));
 // 'lux'
+
+// Альтернативный вариант через распыление, Math.max и получения массивов сразу двумя методами keys и values;
+
+// 1. Создаем массив значений из получаемого обьекта;
+// 2. Создаем массив ключей из получаемого обьекта;
+// 3. Ищем максимальное значение через Math.max в массиве значений;
+// 4. Ищем индекс максимальньного значения в масиве значений;
+// 5. Возвращаем через функцию имя индекса максимального значения;
+
+// const findBestEmployee = function (employees) {
+//   ('use strict');
+//   // Write code under this line
+
+//   const values = Object.values(employees);
+//   const keys = Object.keys(employees);
+//   const max = Math.max(...values);
+
+//   const indexOfMax = values.indexOf(max);
+
+//   return keys[indexOfMax];
+// };
+
+// // Объекты и ожидаемый результат
+// const developers = {
+//   ann: 29,
+//   david: 35,
+//   helen: 1,
+//   lorence: 99,
+// };
+// console.log(findBestEmployee(developers));
+// // 'lorence'
+
+// const supports = {
+//   poly: 12,
+//   mango: 17,
+//   ajax: 4,
+// };
+// console.log(findBestEmployee(supports));
+// // 'mango'
+
+// const sellers = {
+//   lux: 147,
+//   david: 21,
+//   kiwi: 19,
+//   chelsy: 38,
+// };
+// console.log(findBestEmployee(sellers));
+// // 'lux'
