610454fbd60d1f78d172bb1c4ee06038dfc9e684
Dmytro <mityaua@gmail.com>
2020-11-12 17:16:47 +0200

    Добавляет второй вариант решения 2й задачи

--- js/3-2.js
+++ js/3-2.js
@@ -16,3 +16,16 @@ console.log(countProps({})); // 0
 console.log(countProps({ a: 1, b: 1 })); // 2
 
 console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
+
+// Второй вариант решения без дополнительной переменной 
+
+// 1. Обьявляем переменную, которой присваиваем обьект, преобразованный в массив, и сразу же для него определяем длинну.
+// 2. Возвращаем переменную через возврат функции
+
+// const countProps = function (obj) {
+//   'use strict';
+//   // Write code under this line
+//   const keys = Object.keys(obj).length;
+
+//   return keys;
+// };
